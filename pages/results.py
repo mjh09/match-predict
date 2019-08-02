8da085b6bca4f4d220747bce73166cfa144b6cea
Matthew Harshman <mjharshman66@gmail.com>
2019-08-02 02:33:38 -0700

    new img

--- pages/results.py
+++ pages/results.py
@@ -14,12 +14,16 @@ column1 = dbc.Col(
             ## Results
 
             Lets start with the majority classifier. This can be helpful in determining how effective
-            the classifier is. This can be done by normalizing the instances of each class.
-            PLayer A won roughly 63 percent of the time. The model should do better than that.
+            the classifier is. PLayer A won roughly 63 percent of the time. The model should do better than that.
 
             """
         ),
-        html.Img(src='/assets/baseline.PNG', style={'width':'100%','hieght':'100%'})
+        html.Img(src='/assets/newBL.PNG', style={'width':'25%','hieght':'25%'}),
+        dcc.Markdown(
+            """
+
+            """
+        ),
     ],
     md=10,
 )
